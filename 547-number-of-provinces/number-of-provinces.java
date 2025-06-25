c762a9f039083df63d9bc41f715725a7bdf9fc43
AdityaPratap22 <adityapratap221103@gmail.com>
2025-06-25 16:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (55.55%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findCircleNum(int[][] adj) {
+        int n = adj.length;
+        boolean[] visit = new boolean[n];
+        int count = 0;
+        for(int i =0; i<n; i++){
+            if(!visit[i]){
+                dfs(i,visit,adj);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int i, boolean[] visit, int [][] adj){
+        int n= adj.length;
+        visit[i] = true;
+        for(int j=0; j<n;j++){
+            if(!visit[j] && adj[i][j]==1){
+                visit[j] = true;
+                dfs(j,visit,adj);
+            }
+        }
+        return;
+    }
+}
